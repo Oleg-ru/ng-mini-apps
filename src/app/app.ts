a45b394dbad435d9ccb395a4e2e38a5532227de7
oleg <oleg_09-95@mail.ru>
2026-09-16 18:34:53 +0300

    feature: Routing add nav block

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { User } from './components/user/user';
 import { DataBinding } from './components/data-binding/data-binding';
 
 @Component({
-  imports: [RouterOutlet, User, DataBinding],
+  imports: [RouterOutlet, User, DataBinding, RouterLink],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
