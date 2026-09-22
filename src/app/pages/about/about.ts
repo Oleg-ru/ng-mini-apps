8366344a483e72afeacbdbb2c1821dd6c8334202
oleg <oleg_09-95@mail.ru>
2026-09-22 20:19:37 +0300

    feature: Directives attribute: ngStyle

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { NgClass } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [NgClass, FormsModule],
+  imports: [NgClass, FormsModule, NgStyle],
   selector: 'app-about',
   styleUrl: './about.css',
   templateUrl: './about.html',
 })
 export class About {
   isActive = true;
+  textColor = 'green';
 }
