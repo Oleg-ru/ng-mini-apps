53141a407f1d5e8ab75365ecf091c909121c1db6
oleg <oleg_09-95@mail.ru>
2026-09-22 20:26:36 +0300

    feature: Directives structural: ngSwitch

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { NgClass, NgForOf, NgIf, NgStyle } from '@angular/common';
+import { NgClass, NgForOf, NgIf, NgStyle, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [NgClass, FormsModule, NgStyle, NgIf, NgForOf],
+  imports: [NgClass, FormsModule, NgStyle, NgIf, NgForOf, NgSwitch, NgSwitchCase, NgSwitchDefault],
   selector: 'app-about',
   styleUrl: './about.css',
   templateUrl: './about.html',
@@ -14,4 +14,5 @@ export class About {
 
   isLoggedIn = false;
   users = ['Anton', 'Baton', 'Garden'];
+  status = 'active';
 }
