64bd1f5fc11706ac534c7056fb49a4daf6507a2f
oleg <oleg_09-95@mail.ru>
2026-09-22 20:17:56 +0300

    feature: Directives attribute: ngClass

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [NgClass, FormsModule],
   selector: 'app-about',
   styleUrl: './about.css',
   templateUrl: './about.html',
 })
-export class About {}
+export class About {
+  isActive = true;
+}
